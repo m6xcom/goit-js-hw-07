5629a4c7c1ac9fc184d185505633a9b0ceedcde4
m6xcom <65496114+m6xcom@users.noreply.github.com>
2020-11-13 17:33:23 +0200

    Добавляет task-3

---
+++ task-3.js
@@ -0,0 +1,29 @@
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+const gallery = document.querySelector("#gallery");
+const arrayOfImagesLi = images.map((el) => {
+  const createLi = document.createElement("li");
+  createLi.classList.add("galleryItem");
+  const createImg = document.createElement("img");
+  createImg.setAttribute("url", el.url);
+  createImg.setAttribute("alt", el.alt);
+  createLi.append(createImg);
+  return createLi;
+});
+
+gallery.append(...arrayOfImagesLi);
