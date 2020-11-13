b481e49fb4f54f5d9fbe498e81747bc2c30fe293
m6xcom <65496114+m6xcom@users.noreply.github.com>
2020-11-13 18:28:18 +0200

    Добавляет task-4

---
+++ task-4.js
@@ -0,0 +1,18 @@
+const counter = document.querySelector("#value");
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+let counterValue = Number(counter.textContent);
+
+const increment = function () {
+  counterValue += 1;
+  counter.textContent = counterValue;
+  return counterValue;
+};
+const decrement = function () {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+  return counterValue;
+};
+
+incrementBtn.addEventListener("click", () => increment());
+decrementBtn.addEventListener("click", () => decrement());
